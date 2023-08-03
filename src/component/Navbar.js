fc31f8d44afad6e7148091d8d90389b309a49d19
Uday1004 <usolanki068@gmail.com>
2023-08-03 11:25:01 +0530

    link

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,6 +6,7 @@ import {RiTimeLine} from 'react-icons/ri';
 import {RiPhoneFill} from 'react-icons/ri';
 import {RiUserFill} from 'react-icons/ri';
 import {RiHome5Line} from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
@@ -14,28 +15,28 @@ function Navbar() {
       <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/Home">Mausam.UI<img className="weather-icon" src={logo} alt="" /></a>
+    <Link className="navbar-brand" to="/Home">Mausam.UI<img className="weather-icon" src={logo} alt="" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item px-2 ">
-          <a className="nav-link active nav-attribute" aria-current="page" href="/Home" style={{color:'white' , width:'82px'}}>Home <RiHome5Line/></a>
+          <Link className="nav-link active nav-attribute" aria-current="page" to="/Home" style={{color:'white' , width:'82px'}}>Home <RiHome5Line/></Link>
         </li>
         <li className="nav-item px-2 ">
-          <a className="nav-link active nav-attribute" aria-current="page" href="/Map" style={{color:'white' , width:'70.5px'}}>Map <RiMap2Line/></a>
+          <Link className="nav-link active nav-attribute" aria-current="page" to="/Map" style={{color:'white' , width:'70.5px'}}>Map <RiMap2Line/></Link>
         </li>
         <li className="nav-item px-2 " style={{transition: 0.5}}>
-          <a className="nav-link active nav-attribute" href="/Forecast" style={{color:'white', width:'108.5px'}}>Forecaste <RiTimeLine/></a>
+          <Link className="nav-link active nav-attribute" to="/Forecast" style={{color:'white', width:'108.5px'}}>Forecaste <RiTimeLine/></Link>
         </li>
         <li className="nav-item dropdown px-2 ">
-          <a className="nav-link dropdown-toggle active nav-attribute" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:'white'}}>
+          <Link className="nav-link dropdown-toggle active nav-attribute" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:'white'}}>
             Developer <RiCodeBoxFill/>
-          </a>
+          </Link>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Contact <RiPhoneFill/></a></li>
-            <li><a className="dropdown-item" href="/About us">About Us <RiUserFill/></a></li>
+            <li><Link className="dropdown-item" to="#">Contact <RiPhoneFill/></Link></li>
+            <li><Link className="dropdown-item" to="/About us">About Us <RiUserFill/></Link></li>
              
           </ul>
         </li>
